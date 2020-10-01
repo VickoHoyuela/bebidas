b5a3b11e50ba1d675adb93b8b3e4f46ea40cebc2
VictorHoyuela <vickohoyuela15@gmail.com>
2020-10-01 10:23:24 -0500

    Docs folder

---
+++ docs/precache-manifest.6a807e2096fc4536a8bc9e3f41e23fc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6bbf0c27315d8a3f1004f48f1de0d1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93ae7c26ebc9723eec1c",
+    "url": "/static/css/main.eebeedd0.chunk.css"
+  },
+  {
+    "revision": "bae7ceb46783fc1668fb",
+    "url": "/static/js/2.d9def002.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/static/js/2.d9def002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93ae7c26ebc9723eec1c",
+    "url": "/static/js/main.8e41d012.chunk.js"
+  },
+  {
+    "revision": "edce32039fb31cfc4f44",
+    "url": "/static/js/runtime-main.50ddf7b3.js"
+  }
+]);
